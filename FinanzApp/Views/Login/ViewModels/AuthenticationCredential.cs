72acde1b9cb6ac6d6615ae0e6a38d0b519db6e5a
john silvestre <johnkerlin52@gmail.com>
2024-03-17 15:09:25 -0400

    feat: Implementacion de la autenticacion, Creacion de usuarios y login

---
+++ FinanzApp/Views/Login/ViewModels/AuthenticationCredential.cs
@@ -0,0 +1,23 @@
+﻿using Firebase.Auth;
+using Firebase.Auth.Providers;
+using Microsoft.Maui.Controls;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FinanzApp.Views.Login.ViewModels
+{
+	public class AuthenticationCredential
+	{
+		public static FirebaseAuthClient authClient = new FirebaseAuthClient(new FirebaseAuthConfig()
+		{
+			ApiKey = "AIzaSyA0CNu_iB3hTCq6kGxnxZGEsbGoBhrtRNc",
+			AuthDomain = "finanzapp-5bbca.firebaseapp.com",
+			Providers = new FirebaseAuthProvider[]
+						{new EmailProvider()}
+		});
+
+	}
+}
--- FinanzApp/Views/Login/Views/CrearCuenta.xaml.cs
+++ FinanzApp/Views/Login/Views/CrearCuenta.xaml.cs
@@ -1,5 +1,8 @@
 using FinanzApp.Views.Login.Model;
+using FinanzApp.Views.Login.ViewModels;
+using Newtonsoft.Json;
 using Plugin.CloudFirestore;
+using System.ComponentModel;
 
 namespace FinanzApp.Views.Login.Views;
 
@@ -17,17 +20,58 @@ public partial class CrearCuenta : ContentPage
 	}
 	private async void btnVolver_Clicked(object sender, EventArgs e)
 	{
-		await TestInsert();
 		await Navigation.PopAsync();
-		
+
 	}
-	private async Task TestInsert()
+	private async Task CreateAccount()
 	{
+		try
+		{
+			var Iduser = await VMuser.CreateUserAuth(new MuserAuth() { Email = txtEmail.Text, Password = txtPassword.Text });
 
-		await CrossCloudFirestore.Current
-			.Instance
-			.Collection("TEST_Collection")
-			.AddAsync<Muser>(new Muser() { Email = "johnkerlin52@gmail.com", Name ="John"});
+			if (Iduser == "EmailExists")
+			{
+				await DisplayAlert("Cuenta existente:", "Esta cuenta ya existe.", "OK");
+				return;
+			}
 
+			await VMuser.CreateUserTable(new Muser()
+			{
+				Email = txtEmail.Text,
+				Name = txtName.Text,
+				UIDusuario = Iduser
+			});
+		}
+		catch (Exception ex)
+		{
+		}
+	}
+	private async Task<bool> ValidarCamposVacios()
+	{
+		if (!string.IsNullOrEmpty(txtEmail.Text) && !string.IsNullOrEmpty(txtName.Text)&& !string.IsNullOrEmpty(txtPassword.Text) )
+		{
+			if (txtPassword.Text.Length < 6)
+			{
+				await DisplayAlert("Completa:", "Digita una contraseña con al menos 6 digitos", "OK");
+				return false;
+			}
+			else
+			{
+				return true;
+			}
+		}
+		else
+		{
+			await DisplayAlert("Completa:", "Se debe completar los campos anteriores.", "OK");
+			return false;
+		}
+	}
+	private async void btnCrearCuenta_Clicked(object sender, EventArgs e)
+	{
+		if (await ValidarCamposVacios() == true)
+		{
+			await CreateAccount();
+		}
+		
 	}
 }
