b7bf0580ad0eea6f1d577aaac5a2f78e5efe37e6
john silvestre <johnkerlin52@gmail.com>
2024-03-17 00:39:39 -0400

    feat: probando insertar data en el firestore db

--- FinanzApp/Views/Login/Views/CrearCuenta.xaml.cs
+++ FinanzApp/Views/Login/Views/CrearCuenta.xaml.cs
@@ -17,19 +17,17 @@ public partial class CrearCuenta : ContentPage
 	}
 	private async void btnVolver_Clicked(object sender, EventArgs e)
 	{
-		await Navigation.PopAsync();
 		await TestInsert();
+		await Navigation.PopAsync();
+		
 	}
 	private async Task TestInsert()
 	{
-		var models = new Mtest()
-		{
-			Name = "John",
-			Age = 19
-		};
+
 		await CrossCloudFirestore.Current
 			.Instance
 			.Collection("TEST_Collection")
-			.AddAsync<Mtest>(models);
+			.AddAsync<Muser>(new Muser() { Email = "johnkerlin52@gmail.com", Name ="John"});
+
 	}
 }
