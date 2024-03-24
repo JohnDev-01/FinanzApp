178b8948804796197c98669a5952dd362e8d3a80
john silvestre <johnkerlin52@gmail.com>
2024-03-24 11:30:54 -0400

    Create models

---
+++ FinanzApp/Service/Conection.cs
@@ -0,0 +1,15 @@
+﻿using Firebase.Database;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FinanzApp.Service
+{
+	public class Conection
+	{
+		static string urlRealtimeDatabase = "https://finanzapp-5bbca-default-rtdb.firebaseio.com";
+		public static FirebaseClient firebase = new FirebaseClient(urlRealtimeDatabase);
+	}
+}
---
+++ FinanzApp/Views/Transactions/Model/Mtransactions.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FinanzApp.Views.Transactions.Model
+{
+	public class Mtransactions
+	{
+		public int ID { get; set; }
+		public string Iduser { get; set; }
+		public string Tipo { get; set; } // Ingreso o Egreso
+		public DateTime Fecha { get; set; }
+		public decimal Monto { get; set; }
+		public int CategoriaID { get; set; } // ID de la categoría a la que pertenece la transacción
+		public string Descripcion { get; set; }
+	}
+}
