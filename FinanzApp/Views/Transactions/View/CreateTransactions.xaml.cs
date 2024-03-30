1235ac40def0d5f4834b61702bfb5de068e5570a
john silvestre <johnkerlin52@gmail.com>
2024-03-30 13:08:39 -0400

    feat: create design from debit/credit

--- FinanzApp/Views/Transactions/View/CreateTransactions.xaml.cs
+++ FinanzApp/Views/Transactions/View/CreateTransactions.xaml.cs
@@ -1,3 +1,6 @@
+using CommunityToolkit.Maui.Behaviors;
+using FinanzApp.Views.Category.ViewModel;
+
 namespace FinanzApp.Views.Transactions.View;
 
 public partial class CreateTransactions : ContentPage
@@ -5,6 +8,42 @@ public partial class CreateTransactions : ContentPage
 	public CreateTransactions(string type)
 	{
 		InitializeComponent();
+
+	}
+	string number = "";
+
+	protected override async void OnAppearing()
+	{
+		await LoadCategory();
+	}
+	private async Task LoadCategory()
+	{
+		var listCategory = await VMcategory.GetListCategoryFromIdUser();
+		colletionCategory.ItemsSource = listCategory;
+		
+	}
+
+	private void Keyboard_Tapped(object sender, TappedEventArgs e)
+	{
+		try
+		{
+			var numbertyping = ((Frame)sender).AutomationId;
+			
+			if ( number == "0.00")
+			{
+				number = "";
+			}
+			number = numbertyping == "DELETE" ? "0.00" :number + numbertyping;
+			lblAmount.Text = Convert.ToDouble(number).ToString("C");
+		}
+		catch (Exception  )
+		{
+
+		}
+	}
+
+	private void tappedGesture_Tapped(object sender, TappedEventArgs e)
+	{
 
 	}
 }
