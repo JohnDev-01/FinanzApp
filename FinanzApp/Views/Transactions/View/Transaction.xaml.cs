20702cb38d610a1a912e8c7927fd5147c69640e5
john silvestre <johnkerlin52@gmail.com>
2024-03-31 20:14:50 -0400

    feat: diseno inicial

--- FinanzApp/Views/Transactions/View/Transaction.xaml.cs
+++ FinanzApp/Views/Transactions/View/Transaction.xaml.cs
@@ -6,4 +6,46 @@ public partial class Transaction : ContentPage
 	{
 		InitializeComponent();
 	}
+	protected override void OnAppearing()
+	{
+		base.OnAppearing();	
+	}
+
+	private void ValidateDate()
+	{
+		//This method validates that the start date is greater
+		//than the end date, if true then the start date will be the end date
+		try
+		{
+			var dateStart = dtStart.Date;
+			var dateEnd = dtEnd.Date;
+			if (dateStart > dateEnd)
+			{
+				dtStart.Date = dateEnd;
+			}
+		}
+		catch (Exception)
+		{
+		}
+	}
+
+	private void dtEnd_DateSelected(object sender, DateChangedEventArgs e)
+	{
+		ValidateDate();
+	}
+
+	private void dtStart_DateSelected(object sender, DateChangedEventArgs e)
+	{
+		ValidateDate();
+	}
+
+	private void frameStartDate_Tapped(object sender, TappedEventArgs e)
+	{
+		dtStart.Focus();
+	}
+
+	private void framedateEnd_Tapped(object sender, TappedEventArgs e)
+	{
+		dtEnd.Focus();
+	}
 }
